42b78843279d097fa0792aa211356f3effc78b35
Sirui Li <slray@nyu.edu>
2024-02-10 18:22:35 +0400

    added passage vectorizing

---
+++ Interfaces/SteUpInterfaces/PreProcessSN/PassageVectorizingInterface/passage_vectoring_strategy.py
@@ -0,0 +1,15 @@
+# Author: ray
+# Date: 2/10/24
+# Description:
+from typing import Any
+
+
+class PassageVectorizer:
+    def vectorize(self) -> Any:
+        """
+        This function vectorize a passage to a vector
+
+        Returns: vectorized passage
+
+        """
+        raise NotImplementedError("vectorize method must be implemented")
