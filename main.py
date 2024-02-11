f5dccccb143566cc4922ca7c568d9b7b87268888
Sirui Li <slray@nyu.edu>
2024-02-12 00:10:45 +0400

    ready to train on hpc

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # This is a sample Python script.
 from start_training import start_training_process
 if __name__ == '__main__':
-    start_training_process(num_training_steps=10,
-                           batch_size=5,
-                           nhead=1,
-                           transformer_encoder_layer_num=1,
+    start_training_process(num_training_steps=1000,
+                           batch_size=100000,
+                           nhead=4,
+                           transformer_encoder_layer_num=5,
                            num_epochs=10,
                            check_point_steps=1000,
                            dataset_path="data/test_data.csv")
